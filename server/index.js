d7b3f2b27d8134ba5950daae5eff46f73f76afdb
RajatDahiya <dahiyarajat528@gmail.com>
2021-08-10 23:00:42 +0530

    Add Zomato models

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import mongoose from "mongoose";
+
+const zomato = express();
+
+
+//application  middlewares
+zomato.use(express.json());
+zomato.use(express.urlencoded({ extended: false }));
+zomato.use(helmet());
+zomato.use(cors());
+
+zomato.get("/", (req, res) => res.json( { message : "Setup Success" } ));
+
+zomato.listen(4000, () => console.log("server running"));
