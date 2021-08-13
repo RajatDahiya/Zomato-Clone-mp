595022f88b7da0a867e6f4eedb6503ee5373e569
RajatDahiya <dahiyarajat528@gmail.com>
2021-08-13 11:59:50 +0530

    Add Static and Methods

--- server/API/Auth/index.js
+++ server/API/Auth/index.js
@@ -18,29 +18,13 @@ Method  POST
 
 Router.post("/signup", async (req, res) =>{
     try{
-        const { email, password, fullname, phoneNumber } = req.body.credentials;
-
-        // check whether email exists
-        const checkUserByEmail = await UserModel.findOne({ email });
-        const checkUserByPhone = await UserModel.findOne({ phoneNumber });
-
-        if (checkUserByEmail || checkUserByPhone){
-            return res.json({ error: "User already exists!" });
-        }
-
-        // hash the password
-        const bcryptSalt = await bcrypt.genSalt(8);
-
-        const hashedPassword = await bcrypt.hash(password, bcryptSalt);
+        await UserModel.findByEmailAndPhone ( email, phoneNumber);
 
         // save to DB
-        await UserModel.create({
-            ...req.body.credentials,
-            password: hashedPassword
-        });
+        const newUser = await UserModel.create( req.body.credentials );
 
         // generate JWT auth token
-        const token = jwt.sign({user: { Fullname, email } }, "ZomatoAPP");
+        const token = newUser.generateJwtToken ();
 
         // return
         return res.status(200).json({ token });
